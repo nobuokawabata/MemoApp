5234e0b44f14e9a89f5cf2ad81d1ead06d0d4bfe
user01 <nkawabata@dcs.co.jp>
2021-03-15 15:36:16 +0900

    Install ESLint

--- App.jsx
+++ App.jsx
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello, world!</Text>
+      {/* eslint-disable */}
       <StatusBar style="auto" />
     </View>
   );
